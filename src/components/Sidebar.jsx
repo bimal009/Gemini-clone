20191525dae7ebd2ca8d9543bd6e385a00cca6de
bimal009 <pandeybimal616@gmail.com>
2025-02-19 11:25:00 +0545

    changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,17 +18,17 @@ const Sidebar = () => {
             </div>}
 
 
-    {Toogle && <div className='min-h-screen bg bg-[#F0F4F9] w-fit flex flex-col justify-between  py-9 px-8  items-center' >
+    {Toogle && <div className='min-h-screen max-h-screen bg bg-[#F0F4F9] w-fit flex flex-col justify-between  py-9 px-8  items-center ' >
       <div className="icon p flex flex-col justify-center items">
         <div className={Toogle?"icon flex items-center sticky":"icon flex items-center sticky justify-center"}>
 
 
 <img onClick={handleToggle} className='h-[24px] w-[20px] object-contain ' src={assets.menu_icon} alt="" />
         </div>
-        <div className="plus bg-[#E6EAF1]  rounded-full flex items-center justify-center gap-2 mt-9  text-xl px-20 ">
+        <div className="plus bg-[#E6EAF1]  rounded-full flex items-center justify-center gap-2 mt-9  text-xl px-5 py-4">
  
   <img className='h-[24px] w-[20px] object-contain ' src={assets.plus_icon} alt="plusicon" />
-  {Toogle && <p className='font-bold text-[20px]  text-[#989CA0] font-sans '>New Chat</p>}
+  {Toogle && <p className='font-bold align-baseline whitespace-nowrap   text-[#989CA0] font-sans '>New Chat</p>}
       </div>
       <div className="plus  flex  mt-9 items-center
 
@@ -38,7 +38,7 @@ const Sidebar = () => {
       </div>
 </div> 
 
-<div className="bottom flex flex-col gap-4  ">
+<div className="bottom inline-flex flex-col gap-4  justify-center ">
   <div className="help flex gap-4 ">
     <img className='h-[24px] w-[20px] object-contain ' src={assets.question_icon} alt="Help" />{ Toogle &&<p className='font-semibold  '>Help</p>}
   </div>
